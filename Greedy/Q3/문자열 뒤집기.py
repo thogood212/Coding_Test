7d0bcad15f67a1b2f110313f94427eea85f60c7e
Taeho(Damon) <thogood212@gmail.com>
2022-07-15 19:24:27 +0900

    22.07.15

---
+++ Greedy/Q3/문자열 뒤집기.py
@@ -0,0 +1,18 @@
+S=input()
+
+count0=0
+count1=0
+
+if S[0] =='1':
+  count0+=1
+else:
+  count1+=1
+
+for i in range(len(S)-1):
+  if S[i] != S[i+1]:
+    if S[i]=='1':
+      count0+=1
+    else:
+      count1+=1
+
+print(min(count0,count1))
